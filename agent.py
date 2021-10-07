d8e023c3ebd487654cd99386e75345fc48ec1f51
Fergan <50113715+Ferganvj@users.noreply.github.com>
2021-10-07 17:33:37 +0200

    Update agent.py

--- agent.py
+++ agent.py
@@ -4,7 +4,6 @@ import pygame as pg
 AGENT_SIZE = 35, 35
 TRANSPARENT = 0, 0, 0, 0
 
-
 class Agent:
     def __init__(self, pos):
         self.image = self.make_img()
@@ -19,62 +18,9 @@ class Agent:
         img.fill(TRANSPARENT)
         rect = img.get_rect()
 
-        pg.draw.ellipse(img, pg.Color('black'), rect.inflate(-1, -1))
-        pg.draw.ellipse(img, pg.Color('tomato'), rect.inflate(-10, -10))
-
-        return img
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
-
-        # Drawing path from start state to goal state:
-        if self.path:
-            for p in self.path:
-                pg.draw.circle(surface, pg.Color('blue'), p, 5)
-
-    def update(self, dt):
-
-        if self.path:
-            current = self.path[0]
-            self.move_to(current, dt)
-
-            # Update current if we reached it
-            dx = current[0] - self.true_pos[0]
-            dy = current[1] - self.true_pos[1]
-
-            if pg.math.Vector2(dx, dy).length() < 1:
-                self.path.popleft()
-
-    def set_path(self, path):
-        self.path = deque(path)
-
-    def move_to(self, pos, dt):
-        # Calculate distance between current pos and target, and direction
-        vec = pg.math.Vector2(pos[0] - self.true_pos[0], pos[1] - self.true_pos[1])
-        direction = vec.normalize()
-
-        # Progress towards the target
-        self.true_pos[0] += direction[0] * self.speed * dt
-        self.true_pos[1] += direction[1] * self.speed * dt
-
-        self.rect.center = self.true_pos
-# Agent 2 is work in progress
-class Agent2:
-    def __init__(self, pos):
-        self.image = self.make_img()
-        self.rect = self.image.get_rect(center=pos)
-
-        self.true_pos = list(self.rect.center)
-        self.path = None
-        self.speed = 100
-
-    def make_img(self):
-        img = pg.Surface(AGENT_SIZE).convert_alpha()
-        img.fill(TRANSPARENT)
-        rect = img.get_rect()
-
-        pg.draw.ellipse(img, pg.Color('green'), rect.inflate(-1, -1))
-        pg.draw.ellipse(img, pg.Color('pink'), rect.inflate(-10, -10))
+        # Assigning color to the agents start states
+        pg.draw.ellipse(img, pg.Color('black'), rect.inflate(-1, -1))   # Start state color
+        pg.draw.ellipse(img, pg.Color('green'), rect.inflate(-10, -10))
 
         return img
 
@@ -92,7 +38,7 @@ class Agent2:
             current = self.path[0]
             self.move_to(current, dt)
 
-            # Update current if we reached it
+            # Update current when we reached it
             dx = current[0] - self.true_pos[0]
             dy = current[1] - self.true_pos[1]
 
@@ -103,6 +49,7 @@ class Agent2:
         self.path = deque(path)
 
     def move_to(self, pos, dt):
+
         # Calculate distance between current pos and target, and direction
         vec = pg.math.Vector2(pos[0] - self.true_pos[0], pos[1] - self.true_pos[1])
         direction = vec.normalize()
