8eb880335f57a600ce9111eb7e2c37890d8652c2
Fergan <50113715+Ferganvj@users.noreply.github.com>
2021-10-07 17:33:13 +0200

    Update A* algo.py

--- A* algo.py
+++ A* algo.py
@@ -1,4 +1,6 @@
+# Heapq creates empty queue then pushes new item into queue 
 import heapq
+
 
 class PriorityQueue:
     def __init__(self):
@@ -8,10 +10,10 @@ class PriorityQueue:
         return len(self.elements) == 0
 
     def put(self, item, priority):
-        heapq.heappush(self.elements, (priority, item))
+        heapq.heappush(self.elements, (priority, item))     # Pushing item into the queue
 
     def get(self):
-        return heapq.heappop(self.elements)[1]
+        return heapq.heappop(self.elements)[1]      # Popping item out of the queue
 
 
 def reconstruct_path(came_from, start, goal):
@@ -32,6 +34,30 @@ def heuristic(a, b):
 
 
 def a_star_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from, cost_so_far
+
+def a_star_search_2(graph, start, goal):
     frontier = PriorityQueue()
     frontier.put(start, 0)
     came_from = {}
